a87c1ac2d97facca610f97f8ebeb63f3932fd96b
Cam <136983376+Kosiris@users.noreply.github.com>
2023-10-11 19:13:40 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+app.use('/places', require('./controllers/places'))
+app.get('/', (req, res) => {
+    res.send('Hello world!')
+})
+
+app.get('*', (req, res) => {
+    res.status(404).send(`<h1>404 Page</h1>`)
+})
+
+app.listen(process.env.PORT)
+
+app.listen(process.env.PORT)
