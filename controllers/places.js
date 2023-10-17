cbcdfa6797365cc0fcbc0b4ba07a3f9ac1b6251c
Cam <136983376+Kosiris@users.noreply.github.com>
2023-10-16 18:20:16 -0700

    Add files via upload

---
+++ controllers/places.js
@@ -0,0 +1,22 @@
+const router = require('express').Router()
+
+router.get('/', (req,res)=>{
+    let places = [{
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: 'http://placekitten.com/250/250'
+      }, {
+        name: 'Coding Cat Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: 'http://placekitten.com/250/250'
+      }]
+      
+    res.render('places/index', { places })
+
+})
+
+module.exports = router
---
+++ views/places/index.jsx
@@ -0,0 +1,32 @@
+const React = require('react')
+const Def = require('./default')
+
+function index () {
+    return (
+        <Def>
+            <main>
+                <h1>Index</h1>
+            </main>
+        </Def>
+      )
+}
+function index (data) {
+    let placesFormatted = data.places.map((place) => {
+      return (
+        <div>
+          <h2>{place.name}</h2>
+          <img src={place.pic} alt={place.name}/>
+        </div>
+      )
+    })
+    return (
+      <Def>
+          <main>
+              <h1>PLACES INDEX PAGE</h1>
+              {placesFormatted}
+          </main>
+      </Def>
+  )
+  }
+  
+module.exports = index
