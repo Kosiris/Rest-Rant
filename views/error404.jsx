55c9f9607820aff96bec0f7e145a4d87446b1769
Cam <farrcameron@gmail.com>
2023-10-21 10:10:01 -0700

    updates

--- views/error404.jsx
+++ views/error404.jsx
@@ -6,6 +6,12 @@ function error404 () {
       <Def>
           <main>
               <h1>404: PAGE NOT FOUND</h1>
+              <div>
+                  <img src="https://placekitten.com" alt="Placekitten.com" />
+                </div>
+                <div>
+                Photo by <a href="https://placekitten.com">Mark James</a> on <a href="https://placekitten.com">Placekitten.com</a>
+                </div>
               <p>Oops, sorry, we can't find this page!</p>
           </main>
       </Def>
--- views/home.jsx
+++ views/home.jsx
@@ -3,12 +3,21 @@ const Def = require('./default')
 
 function home () {
     return (
-      <Def>
-          <main>
-              <h1>HOME</h1>
-          </main>
-      </Def>
+        <Def>
+            <main>
+                <h1>HOME</h1>
+                <div>
+                  <img src="/images/chia-fruit-drink.jpg" alt="Chia Fruit Shake" />
+                </div>
+                <div>
+                Photo by <a href="https://unsplash.com/@ellaolsson?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Ella Olsson</a> on <a href="https://unsplash.com/photos/assorted-fruits-and-vegetable-on-brown-wooden-chopping-board-2IxTgsgFi-s?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+                </div>
+                <a href="/places">
+                    <button className="btn-primary">Places Page</button>
+                </a>
+            </main>
+        </Def>
     )
-  }
-  
+}
+
 module.exports = home
