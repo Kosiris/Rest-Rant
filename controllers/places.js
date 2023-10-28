afbed4ba684c7cf61761bd88306a4625c473d9e8
Cam <farrcameron@gmail.com>
2023-10-28 09:31:52 -0700

    Update

--- controllers/places.js
+++ controllers/places.js
@@ -18,7 +18,7 @@ router.get('/:id', (req, res) => {
     res.render('error404')
   }
   else {
-    res.render('places/show', { place: places[id] })
+    res.render('places/show', { place: places[id], id })
   }
 })
 
@@ -48,6 +48,22 @@ router.post('/', (req, res) => {
   places.push(req.body)
   res.redirect('/places')
 })
+
+router.delete('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    places.splice(id, 1)
+    res.redirect('/places')
+  }
+})
+
+
 
 
 module.exports = router
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -6,9 +6,17 @@ function show () {
         <Def>
           <main>
             <h1>{data.place.name}</h1>
+            <h2>Currently Unrated</h2>
+            <a href={`/places/${data.id}/edit`} className="btn btn-warning">Edit</a>  
+            <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+            <button type="submit" className="btn btn-danger">Delete</button>
+            </form>     
+
           </main>
         </Def>
     )
 }
+
+
 
 module.exports = show
