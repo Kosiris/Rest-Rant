3769eeae83e84e1c034755ce6da1e5bd117865e5
Cam <farrcameron@gmail.com>
2023-10-30 18:08:59 -0700

    Update

--- controllers/places.js
+++ controllers/places.js
@@ -25,15 +25,29 @@ router.get('/:id/edit', (req, res) => {
 
 
 router.put('/:id', (req, res) => {
-  db.Place.findByIdAndUpdate(req.params.id, req.body)
-      .then(() => {
-          res.redirect(`/places/${req.params.id}`)
-      })
-      .catch(err => {
-          console.log('err', err)
-          res.render('error404')
-      })
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+      if (!req.body.pic) {
+          // Default image if one is not provided
+          req.body.pic = 'http://placekitten.com/400/400'
+      }
+      if (!req.body.city) {
+          req.body.city = 'Anytown'
+      }
+      if (!req.body.state) {
+          req.body.state = 'USA'
+      }
+      places[id] = req.body
+      res.redirect(`/places/${id}`)
+  }
 })
+
 
 router.post('/', (req, res) => {
   console.log(req.body)
